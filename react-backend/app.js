9d8901fad89bad883f0fb495aca2c2eb6d2de265
Robert Quan <rob.quan@gmail.com>
2018-01-27 15:39:04 -0700

    Tweaked server in backend to run on 3001 by defualt, instead of 3000, and Nodemon will start server.

--- react-backend/app.js
+++ react-backend/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const port = process.env.PORT || '3001'
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -42,5 +42,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, ()=>{
+  console.log(`you are now tunned to ${port} "the bees knees"`)
+})
 
 module.exports = app;
