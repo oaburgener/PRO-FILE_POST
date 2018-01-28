078ec038171242936034060953256a3ba9d519f1
Robert Quan <rob.quan@gmail.com>
2018-01-28 14:06:23 -0700

    Added Material UI, navbar started

--- pro-file-post/src/App.js
+++ pro-file-post/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Body from './components/Body'
 import Footer from './components/Footer'
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <MuiThemeProvider>
+          <Navbar />
+        </MuiThemeProvider>
         <Body />
         <Footer />
       </div>
--- pro-file-post/src/components/Navbar.js
+++ pro-file-post/src/components/Navbar.js
@@ -1,34 +1,64 @@
-import React from 'react'
+import React from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import Menu from 'material-ui/svg-icons/navigation/menu';
 
 
-export default class Navabar extends React.Component {
-render(){
-  return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      </div>
-  )
-}
+export default class ToolbarExamplesSimple extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 3,
+    };
+  }
+
+  handleChange = (event, index, value) => this.setState({value});
+
+  render() {
+    return (
+      <Toolbar>
+
+        <ToolbarGroup firstChild={true}>
+          <ToolbarTitle text="Options" />
+          <FontIcon className="muidocs-icon-custom-sort" />
+          <ToolbarSeparator />
+          <RaisedButton label="Create Broadcast" primary={true} />
+          <IconMenu
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationExpandMoreIcon />
+              </IconButton>
+            }
+          >
+            <MenuItem primaryText="Download" />
+            <MenuItem primaryText="More Info" />
+          </IconMenu>
+        </ToolbarGroup>
+        <ToolbarGroup>
+
+          <IconMenu
+            iconButtonElement={<IconButton><Menu /></IconButton>}
+            onChange={this.handleChangeSingle}
+            value={this.state.valueSingle}
+          >
+
+            <MenuItem value={1} primaryText="All Broadcasts" />
+            <MenuItem value={2} primaryText="All Voice" />
+            <MenuItem value={3} primaryText="All Text" />
+            <MenuItem value={4} primaryText="Complete Voice" />
+            <MenuItem value={5} primaryText="Complete Text" />
+            <MenuItem value={6} primaryText="Active Voice" />
+            <MenuItem value={7} primaryText="Active Text" />
+          </IconMenu>
+        </ToolbarGroup>
+      </Toolbar>
+    );
+  }
 }
